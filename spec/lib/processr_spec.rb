a8f6b83e666945dd9ded35e28cdb68abe24a19e6
Josh Nesbitt <josh@josh-nesbitt.net>
2010-07-13 15:15:54 +0100

    Added new specs for updated (backwards incompatible) API.

--- spec/lib/processr_spec.rb
+++ spec/lib/processr_spec.rb
@@ -1,10 +1,11 @@
 describe Processr do
   
-  before :all do
+  before :each do
     Processr.configure do |config|
       config.root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-      config.out  = File.join(config.root, 'examples', 'all')
     end
+    
+    @processor = Processr.new
   end
   
   it "should successfully accept a configuration block" do
@@ -20,7 +21,7 @@ describe Processr do
     Processr.out.should be_nil
     
     root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-    out  = File.join(root, 'examples', 'all')
+    out  = File.join(root, '..', 'examples', 'all')
     
     Processr.configure do |config|
       config.root = root
@@ -29,6 +30,52 @@ describe Processr do
     
     Processr.root.should == root
     Processr.out.should  == out
+  end
+  
+  it "should allow text to be added to the buffer" do
+    @processor.buffer.should == ""
+    @processor << "Some Text"
+    @processor.buffer.should == "Some Text"
+  end
+  
+  it "should read files to the buffer if no output file is provided" do
+    Processr.out = nil
+    @processor.buffer.should == ""
+    @processor.files << File.join('fixtures', 'one.txt')
+    @processor.files << File.join('fixtures', 'two.txt')
+    @processor.process!.should == "one\ntwo\n"
+  end
+  
+  it "should write files from the buffer if an output file is given" do
+    Processr.out = File.join(Processr.root, 'fixtures', 'output.txt')
+    
+    @processor.buffer.should == ""
+    @processor.files << File.join('fixtures', 'one.txt')
+    @processor.process!.should be_true
+    
+    File.exists?(File.join(Processr.root, 'fixtures', 'output.txt')).should == true
+    File.open(File.join(Processr.root, 'fixtures', 'output.txt')).read.should == "one\n"
+  end
+  
+  it "should allow the use of filters to modify output" do
+    Processr.out = nil
+    
+    LanguageFilter = lambda do |buffer|
+      lookup = [
+        "fucking",
+        "hate"
+      ]
+      
+      lookup.each do |word|
+        buffer.gsub!(word, ("*" * word.size))
+      end
+      
+      buffer
+    end
+    
+    @processor.add_filter(LanguageFilter)
+    @processor << "I fucking hate bad language."
+    @processor.process!.should == "I ******* **** bad language."
   end
   
 end
