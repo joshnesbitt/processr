1fdeecdb1d1c3b41f2a3b2b49a01f2b49eb37128
Josh Nesbitt <josh@josh-nesbitt.net>
2010-07-13 15:17:00 +0100

    Added code to make tests on the new API pass.

--- lib/processr.rb
+++ lib/processr.rb
@@ -1,6 +1,7 @@
 $:.unshift File.dirname(__FILE__)
 
 class Processr
+  
   class << self
     def root=(path); @root = path; end
     def root; @root; end
@@ -13,10 +14,11 @@ class Processr
     end
   end
   
-  attr_accessor :buffer, :filters
+  attr_accessor :buffer, :files, :filters
   
   def initialize
     self.buffer  = ""
+    self.files   = []
     self.filters = []
   end
   
@@ -25,23 +27,41 @@ class Processr
   end
   
   def process!
-    self.filters.each do |filter|
-      self.buffer = filter.call(self.buffer)
-    end
-    
-    write_buffer!
-    
-    true
+    read_files
+    process_filters
+    write_output
   end
   
-  def <<(file)
-    self.buffer << File.open(File.join(self.class.root, file)).read
+  def <<(text)
+    self.buffer << text
   end
   
   private
-  def write_buffer!
+  def read_files
+    self.files.each do |file|
+      read_file_to_buffer(file)
+    end
+  end
+  
+  def read_file_to_buffer(file)
+    self.buffer << File.open(File.join(self.class.root, file)).read
+  end
+  
+  def process_filters
+    self.filters.each do |filter|
+      self.buffer = filter.call(self.buffer)
+    end
+  end
+  
+  def write_output
+    self.class.out.nil? ? self.buffer : write_buffer_file
+  end
+  
+  def write_buffer_file
     output_file = File.open(self.class.out, "w+")
     output_file.puts self.buffer
     output_file.close
+    
+    true
   end
 end
